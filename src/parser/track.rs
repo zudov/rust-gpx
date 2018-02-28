a53517358d45cbc3c85aa3606b3d95ba9e9b1104
Konstantin Zudov <co@zudov.me>
2018-02-28 11:09:04 +0200

    Skip over the <extensions> tag and its children

--- src/parser/track.rs
+++ src/parser/track.rs
@@ -14,6 +14,7 @@ use Track;
 /// consume consumes a GPX track from the `reader` until it ends.
 pub fn consume<R: Read>(reader: &mut Peekable<Events<R>>) -> Result<Track> {
     let mut track: Track = Default::default();
+    let mut consuming_extensions: bool = false;
 
     while let Some(event) = reader.next() {
         match event.chain_err(|| "error while parsing XML")? {
@@ -25,14 +26,22 @@ pub fn consume<R: Read>(reader: &mut Peekable<Events<R>>) -> Result<Track> {
                 "src" => track.source = Some(string::consume(reader)?),
                 "type" => track._type = Some(string::consume(reader)?),
                 "trkseg" => track.segments.push(tracksegment::consume(reader)?),
-                child => Err(Error::from(ErrorKind::InvalidChildElement(
-                    String::from(child),
-                    "track",
-                )))?,
+                "extensions" => consuming_extensions = true,
+                _  if consuming_extensions => {},
+                child =>
+                    Err(Error::from(ErrorKind::InvalidChildElement(
+                      String::from(child),
+                      "track",
+                    )))?,
+
             },
 
             XmlEvent::EndElement { .. } => {
-                return Ok(track);
+                if consuming_extensions {
+                    consuming_extensions = false;
+                } else {
+                    return Ok(track);
+                }
             }
 
             _ => {}
@@ -53,12 +62,25 @@ mod tests {
     fn consume_full_track() {
         let track = consume!(
             "
-            <trk>
+            <trk xmlns=\"http://www.topografix.com/GPX/1/1\"
+                 xmlns:gpxx=\"http://www.garmin.com/xmlschemas/GpxExtensions/v3\"
+                 xmlns:gpxtrkx=\"http://www.garmin.com/xmlschemas/TrackStatsExtension/v1\">
                 <name>track name</name>
                 <cmt>track comment</cmt>
                 <desc>track description</desc>
                 <src>track source</src>
                 <type>track type</type>
+                <extensions>
+                  <gpxx:TrackExtension>
+                    <gpxx:DisplayColor>Blue</gpxx:DisplayColor>
+                  </gpxx:TrackExtension>
+                  <gpxtrkx:TrackStatsExtension>
+                    <gpxtrkx:Distance>60986</gpxtrkx:Distance>
+                    <gpxtrkx:TotalElapsedTime>88210</gpxtrkx:TotalElapsedTime>
+                    <gpxtrkx:MovingTime>14117</gpxtrkx:MovingTime>
+                    <gpxtrkx:StoppedTime>4865</gpxtrkx:StoppedTime>
+                  </gpxtrkx:TrackStatsExtension>
+                </extensions>
             </trk>
             "
         );
